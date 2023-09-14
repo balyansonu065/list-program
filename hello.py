c37cd718020c810f222ec034039109ad990a4b71
Sonu <142866219+balyansonu065@users.noreply.github.com>
2023-09-14 15:20:03 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 
 # Creating a List with
 # the use of Numbers
-# (Having duplicate values)
+
 List = [1, 2, 4, 4, 3, 3, 3, 6, 5]
 print("\nList with the use of Numbers: ")
 print(List)
  
 # Creating a List with
 # mixed type of values
-# (Having numbers and strings)
+
 List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
 print("\nList with the use of Mixed Values: ")
 print(List)
